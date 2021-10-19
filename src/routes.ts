b73c2356dc0d25ddb9cb68859c58c70ab263ecc9
Yuri Gonçalves Lima <yurigoncalveslima33@outlook.com>
2021-10-19 12:31:14 -0300

    Rota para gerar token JWT

--- src/routes.ts
+++ src/routes.ts
@@ -1,12 +1,19 @@
 import { Router } from 'express';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
 import { CreateTagController } from './controllers/CreateTagController';
 import { CreateUserController } from './controllers/CreateUserController';
 import { ensureAdmin } from './middlewares/ensureAdmin';
 
 const router = Router();
 
+const authenticateController = new AuthenticateUserController();
 const createUserController = new CreateUserController();
 const createTagController = new CreateTagController();
+
+/**
+ * Authentication
+ */
+router.post('/login', authenticateController.handle);
 
 /**
  * User
